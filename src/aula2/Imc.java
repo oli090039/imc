e2f6902b637873ddb6c4775dc688bd44411a8735
Celso Furtado <celso.alunos@cfp127aluno.local>
2024-05-08 14:56:42 -0300

    Versão 1

---
+++ src/aula2/Imc.java
@@ -0,0 +1,22 @@
+package aula2;
+
+public class Imc {
+
+    public static void main(String[] args) {
+
+        int peso = 73;
+        double altura = 1.74;
+
+        double imc;
+
+        imc = peso / (altura * altura);
+
+
+
+        System.out.println(imc);
+
+
+
+    }
+
+}
